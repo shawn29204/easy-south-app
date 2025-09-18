7aa4f32b748084bfd06d9f57d055eca9298a9ab1
Shawn Campbell <campbsh@mac.mynetworksettings.com>
2025-09-18 14:41:50 -0400

    Fix: Add polyfills for Vercel build

---
+++ config-overrides.js
@@ -0,0 +1,25 @@
+// config-overrides.js
+const webpack = require('webpack');
+
+module.exports = function override(config, env) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    "stream": require.resolve("stream-browserify"),
+    "crypto": require.resolve("crypto-browserify"),
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "os": require.resolve("os-browserify/browser"),
+    "url": require.resolve("url"),
+    "buffer": require.resolve("buffer"),
+    "assert": require.resolve("assert")
+  };
+  
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ]);
+
+  return config;
+};
